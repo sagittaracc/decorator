64ecf4bd5f38ed9d70e44ed33c7150e53292ed93
arutyunyan <arutyunyan@bolid.ru>
2023-01-11 17:36:47 +0300

    refactor

--- tests/classes/Calc.php
+++ tests/classes/Calc.php
@@ -5,7 +5,6 @@ namespace Sagittaracc\PhpPythonDecorator\tests\classes;
 use Sagittaracc\PhpPythonDecorator\Decorator\Decorator;
 use Sagittaracc\PhpPythonDecorator\tests\decorators\Log;
 use Sagittaracc\PhpPythonDecorator\tests\decorators\Timer;
-use Sagittaracc\PhpPythonDecorator\tests\decorators\Retry;
 
 class Calc
 {
@@ -21,17 +20,5 @@ class Calc
     protected function sum2($a, $b)
     {
         return $a + $b;
-    }
-
-    #[Retry(2)]
-    protected function div1($a, $b)
-    {
-        return $a / $b;
-    }
-
-    #[Retry(3)]
-    protected function div2($a, $b)
-    {
-        return $a / $b;
     }
 }
