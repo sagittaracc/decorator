b56413ecb38f1faad1197e313975e4fae84d8fdb
arutyunyan <arutyunyan@bolid.ru>
2023-02-22 15:20:59 +0300

    refactor

--- tests/OrmTest.php
+++ tests/OrmTest.php
@@ -12,17 +12,17 @@ final class OrmTest extends TestCase
         /**
          * Orm example
          */
-        $category = Category::findOne(1);
-        $products = $category->_products;
+        // $category = Category::findOne(1);
+        // $products = $category->_products;
 
-        $firstProduct = $products[0];
-        $secondProduct = $products[1];
+        // $firstProduct = $products[0];
+        // $secondProduct = $products[1];
 
-        $this->assertSame(1, $firstProduct->id);
-        $this->assertSame('computer', $firstProduct->name);
+        // $this->assertSame(1, $firstProduct->id);
+        // $this->assertSame('computer', $firstProduct->name);
 
-        $this->assertSame(2, $secondProduct->id);
-        $this->assertSame('phone', $secondProduct->name);
+        // $this->assertSame(2, $secondProduct->id);
+        // $this->assertSame('phone', $secondProduct->name);
         
         $this->assertNull(null);
     }
