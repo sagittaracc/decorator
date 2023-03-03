ea233b9ca4051a8b4ccea4e26d7d2ae362bacc9d
arutyunyan <arutyunyan@bolid.ru>
2023-03-03 10:31:41 +0300

    refactor

--- tests/DecoratorTest.php
+++ tests/DecoratorTest.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use Sagittaracc\PhpPythonDecorator\tests\decorators\Double;
 use Sagittaracc\PhpPythonDecorator\tests\decorators\Timer;
 
 final class DecoratorTest extends TestCase
@@ -11,23 +10,13 @@ final class DecoratorTest extends TestCase
     public function testDecorator(): void
     {
         $timer = new Timer();
-        $double = new Double();
 
         $result = $timer->wrapper(
-
-            function () use ($double) {
-
-                return $double->wrapper(
-
-                    function () {
-                        return 1 + 2;
-                    }
-                );
-
+            function () {
+                return 1 + 2;
             }
-
         );
 
-        $this->assertSame('Total execution: 1; Result: 6', $result);
+        $this->assertSame('Total execution: 1; Result: 3', $result);
     }
 }
