fabe9d80e8312777894f7cd08a40e4b50170223d
arutyunyan <arutyunyan@bolid.ru>
2023-03-17 14:27:45 +0300

    refactor

--- src/helpers/names.php
+++ src/helpers/names.php
@@ -1,11 +1,27 @@
 <?php
 
-function get_real_name($name)
-{
-    return trim($name, '_');
-}
+/**
+ * @author Yuriy Arutyunyan <sagittaracc@gmail.com>
+ */
 
+/**
+ * Данная функция генерирует имя для метода или свойства
+ * так чтобы при обращении к нему применялись его декораторы
+ * @param string $name
+ * @return string
+ */
 function get_decor_name($name)
 {
     return '_' . $name . '_';
+}
+/**
+ * Данная функция возвращает реальное имя метода или свойства
+ * которое должно быть вызвано при обращении к нему по
+ * декорированному имени метода или свойства
+ * @param string $name
+ * @return string
+ */
+function get_real_name($name)
+{
+    return trim($name, '_');
 }
