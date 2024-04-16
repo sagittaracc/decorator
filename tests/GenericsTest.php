597e2e94e5e7c9f77d9c72ba702c827dacd24cca
arutyunyan <arutyunyan@bolid.ru>
2024-04-16 14:13:38 +0300

    refactor

--- tests/GenericsTest.php
+++ tests/GenericsTest.php
@@ -7,40 +7,14 @@ use Sagittaracc\PhpPythonDecorator\exceptions\GenericError;
 use Sagittaracc\PhpPythonDecorator\modules\generics\Generics;
 use Sagittaracc\PhpPythonDecorator\modules\generics\primitives\Number;
 use Sagittaracc\PhpPythonDecorator\modules\generics\primitives\Str;
-use Sagittaracc\PhpPythonDecorator\tests\examples\Box;
 use Sagittaracc\PhpPythonDecorator\tests\examples\MyAnotherBox;
 use Sagittaracc\PhpPythonDecorator\tests\examples\MyBox;
 use Sagittaracc\PhpPythonDecorator\tests\examples\Pen;
-use Sagittaracc\PhpPythonDecorator\tests\examples\Pencil;
 use Sagittaracc\PhpPythonDecorator\tests\generics\T;
 use Sagittaracc\PhpPythonDecorator\tests\generics\U;
 
 final class GenericsTest extends TestCase
 {
-    public function testBoxItemsGenericException(): void
-    {
-        $this->expectException(GenericError::class);
-
-        $box = new Box();
-        $box(Pencil::class);
-
-        $pencil = new Pencil();
-        $pen = new Pen();
-        $box->_items = [$pencil, $pen];
-    }
-
-    public function testBoxItems(): void
-    {
-        $box = new Box();
-        $box(Pen::class);
-
-        $pen1 = new Pen();
-        $pen2 = new Pen();
-        $box->_items = [$pen1, $pen2];
-
-        $this->assertTrue(true);
-    }
-
     public function testGenericModule(): void
     {
         $box = new MyBox();
