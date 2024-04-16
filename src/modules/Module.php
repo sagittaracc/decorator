0a9ecccede77e41783f6c4165308ffe8bfadae7e
arutyunyan <arutyunyan@bolid.ru>
2024-04-16 14:21:56 +0300

    refactor

--- src/modules/Module.php
+++ src/modules/Module.php
@@ -2,12 +2,18 @@
 
 namespace Sagittaracc\PhpPythonDecorator\modules;
 
+use Sagittaracc\PhpPythonDecorator\exceptions\ModuleError;
+
 class Module
 {
     protected object $object;
 
     public static function install(object $object)
     {
+        if (!isset($object->scope)) {
+            throw new ModuleError;
+        }
+
         if (isset($object->scope['modules'][static::class])) {
             $instance = $object->scope['modules'][static::class]['instance'];
         }
