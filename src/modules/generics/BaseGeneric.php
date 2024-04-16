1bbd8b408086577fbcde0eb00660d10fd84d3763
arutyunyan <arutyunyan@bolid.ru>
2024-04-16 11:53:14 +0300

    add comment how to check generic

--- src/modules/generics/BaseGeneric.php
+++ src/modules/generics/BaseGeneric.php
@@ -7,9 +7,20 @@ use Sagittaracc\PhpPythonDecorator\PythonDecorator;
 
 class BaseGeneric extends PythonDecorator
 {
-    public function wrapper(mixed $object)
+    private function checkGeneric($value)
     {
-        $generics = Generics::install($object);
+        // $index = index_of(static::class, $this->getObject()->scope['modules'][Generics::class]['generics'])
+        // $entity = $box->scope['modules'][Generics::class]['entities'][$index]
+        // if Validation then check validation
+        // else value or value item instance of entity
+        return true;
+    }
+
+    public function wrapper(mixed $object_or_value)
+    {
+        $this->checkGeneric($object_or_value);
+
+        $generics = Generics::install($object_or_value);
         $generics->addName(static::class);
 
         return fn(...$args) => $generics->addEntities($args);
