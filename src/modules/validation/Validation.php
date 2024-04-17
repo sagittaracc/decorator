4843e1a472369fe6007cfd0c8ff3dc0bebb028f3
arutyunyan <arutyunyan@bolid.ru>
2024-04-17 11:40:19 +0300

    Начало реализации модуля валидации

--- src/modules/validation/Validation.php
+++ src/modules/validation/Validation.php
@@ -5,4 +5,14 @@ namespace Sagittaracc\PhpPythonDecorator\modules\validation;
 use Sagittaracc\PhpPythonDecorator\modules\Module;
 
 class Validation extends Module
-{}
+{
+    public function addError($prop, $error)
+    {
+        $this->object->scope['modules'][self::class]['properties'][$prop]['errors'][] = $error;
+    }
+
+    public function addWarning($prop, $warning)
+    {
+        $this->object->scope['modules'][self::class]['properties'][$prop]['warnings'][] = $warning;
+    }
+}
