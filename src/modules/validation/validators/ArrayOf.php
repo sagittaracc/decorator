9d467fd0bff4e6755196995734b883c620ef9602
arutyunyan <arutyunyan@bolid.ru>
2024-04-19 13:14:39 +0300

    refactor array of validator

--- src/modules/validation/validators/ArrayOf.php
+++ src/modules/validation/validators/ArrayOf.php
@@ -4,10 +4,10 @@ namespace Sagittaracc\PhpPythonDecorator\modules\validation\validators;
 
 use Attribute;
 use Sagittaracc\PhpPythonDecorator\modules\generics\core\Generic;
-use Sagittaracc\PhpPythonDecorator\PythonDecorator;
+use Sagittaracc\PhpPythonDecorator\modules\validation\Validator;
 
 #[Attribute]
-class ArrayOf extends PythonDecorator
+class ArrayOf extends Validator
 {
     protected mixed $of;
 
@@ -16,21 +16,27 @@ class ArrayOf extends PythonDecorator
         $this->of = $of;
     }
 
-    public function wrapper(mixed $prop)
+    public function validation($value)
     {
+        if (!is_array($value)) {
+            return false;
+        }
+
+        // ...
         $of = new $this->of;
 
         if ($of instanceof Generic)
         {
             $of->bindTo($this->getObject());
 
-            foreach ($prop as $item)
-            {
+            foreach ($value as $item) {
                 $of->wrapper($item);
             }
         }
         else {
             // TODO: $of instanceof PrimitiveInterface (e.g. Number, String, Boolean)
         }
+
+        return true;
     }
 }
